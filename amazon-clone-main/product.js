fd34ac468c553b0cd945b7e0c15d161c819f3835
K Sriram Sathish <sathishkunapureddy@gmail.com>
2025-11-04 22:48:06 +0530

    Add files via upload

---
+++ amazon-clone-main/product.js
@@ -0,0 +1,5 @@
+// products.js
+export const products = [
+  {id: 1, name: "Men’s T-Shirt", price: 499, img: "images/tshirt.jpg"},
+  {id: 2, name: "Bluetooth Speaker", price: 1499, img: "images/speaker.jpg"},
+];
